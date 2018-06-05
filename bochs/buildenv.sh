31a946b1b6f57b8851ca8e100b18faf28cafa6f3
yifengyou <842056007@qq.com>
2018-06-05 12:44:30 +0800

    modify buildenv.sh

---
+++ bochs/buildenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install build-essential libgtk2.0-dev xorg-dev bison libx11-dev nasm  -y
+
--- code/buildenv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo apt-get update
-sudo apt-get install nasm -y
-sudo apt-get install bochs* -y
-
